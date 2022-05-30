9817531ef53d415a0871033bf72eaf1cf0764d92
mezochka <99035329+mezochka@users.noreply.github.com>
2022-05-30 19:27:20 +0000

    Deploying to gh-pages from @ mezochka/goit-team7-project@6a850fd6c8502f63502997d6fe44e8c0fb6489b2 🚀

---
+++ index.1b704b2b.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-two-open]"),closeModalBtn:document.querySelector("[data-modal-two-close]"),modal:document.querySelector("[data-modal-two]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.1b704b2b.js.map
