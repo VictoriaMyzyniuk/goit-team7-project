bd5cfa52337f5b65db585dce95ca0636ec83a1c6
mezochka <99035329+mezochka@users.noreply.github.com>
2022-05-31 11:25:11 +0000

    Deploying to gh-pages from @ mezochka/goit-team7-project@992fe31fb7ccd4f80b38f08ef88c6ae6eae87b14 🚀

--- index.59820399.js
+++
@@ -1,2 +0,0 @@
-(()=>{const o={openModalBtn:document.querySelector("[data-modal-two-open]"),closeModalBtn:document.querySelector("[data-modal-two-close]"),modal:document.querySelector("[data-modal-two]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
-//# sourceMappingURL=index.59820399.js.map
--- index.5dbd61e8.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-two-open]"),closeModalBtn:document.querySelector("[data-modal-two-close]"),modal:document.querySelector("[data-modal-two]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.5dbd61e8.js.map
